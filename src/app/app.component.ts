17d51453dc634c8484b007b0658bc50acb9ef520
Guillermo Manterola Rojas <guillermo.manterola@raona.com>
2020-05-15 17:52:05 +0200

    acabado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,17 +4,16 @@ import { ValoremonService } from '../app/valoremon.service'
 import { NgForm, FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
+declare var M: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent implements OnInit {
   title = 'valoremon';
-
-
-
-
+  
   constructor(private valoremonservice: ValoremonService, private router: Router) { }
 
   ngOnInit(): void {
@@ -23,13 +22,34 @@ export class AppComponent implements OnInit {
   }
 
 
- enviar( ){}
-  
+  enviar(name: string, type: string, HP: string, Attack: string, Defense: string, SpceialAttack: string, SpecialDefense: string, Speed: string, Abilities: string
+  ) {
+    const stats = {
+      'type': type,
+      'Abilities': Abilities,
+      'Hp': HP,
+      'Attack': Attack,
+      'Defense': Defense,
+      'SpecialAttack': SpceialAttack,
+      'SpecialDefense': SpecialDefense,
+      'Speed': Speed
+    }
+
+    this.valoremonservice.getTier(stats)
+      .subscribe(res => {
+
+        if (res != null) {
+          var result = res.toString();
+          var resultSPlit = result.split("'");
+
+          console.log(resultSPlit[1]);
+
+          var finalResult = resultSPlit[1];
+          M.toast({html:" Tu Pokémon pertenece al tier : " + finalResult });
+        }
+      })
 
 
-
-
- name: string, type: string, HP: string, Attack: string, Defense: string, SpceialAttack: string, SpecialDefense: string, Speed: string, Abilities: string
-
+  }
 
 }
--- src/app/valoremon.service.ts
+++ src/app/valoremon.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { getInterpolationArgsLength } from '@angular/compiler/src/render3/view/util';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ValoremonService {
 
-  constructor() { }
+
+  readonly URL_API = 'http://localhost:2000';
+
+  constructor(private http: HttpClient) {}
+
+  getTier(stats: any){
+
+    return this.http.post(this.URL_API + '/gettier', stats);
+
+  }
 }
